250840abf5de4fa5343e3e54aafa4dbd10e28ad6
Asami Tanii <g033.9521@gmail.com>
2018-04-23 15:30:15 -0700

    implemented roll command to let user re-roll as often as needed

--- Assignment4(Threelow)/main.m
+++ Assignment4(Threelow)/main.m
@@ -19,20 +19,30 @@ int main(int argc, const char * argv[]) {
         Dice *dice4 = [Dice new];
         Dice *dice5 = [Dice new];
         
-        // randomize their values and print them
-        int dice1Value = [dice1 randomizeValue];
-        int dice2Value = [dice2 randomizeValue];
-        int dice3Value = [dice3 randomizeValue];
-        int dice4Value = [dice4 randomizeValue];
-        int dice5Value = [dice5 randomizeValue];
-        
-        [InputCollector printToPrompt: [dice1 convertValueToUnicodeSymbols:dice1Value]];
-        [InputCollector printToPrompt: [dice2 convertValueToUnicodeSymbols:dice2Value]];
-        [InputCollector printToPrompt: [dice3 convertValueToUnicodeSymbols:dice3Value]];
-        [InputCollector printToPrompt: [dice4 convertValueToUnicodeSymbols:dice4Value]];
-        [InputCollector printToPrompt: [dice5 convertValueToUnicodeSymbols:dice5Value]];
-        
-        
+        // When a user inputs the word roll, randomize the values and print them
+        NSString *user_input = @"";
+        int flag = 0;
+        while (flag == 0) {
+            user_input = [InputCollector inputForPrompt:@"> "];
+            if ([user_input isEqualToString:@"roll"]) {
+                // randomize their values
+                int dice1Value = [dice1 randomizeValue];
+                int dice2Value = [dice2 randomizeValue];
+                int dice3Value = [dice3 randomizeValue];
+                int dice4Value = [dice4 randomizeValue];
+                int dice5Value = [dice5 randomizeValue];
+                
+                // and print them
+                [InputCollector printToPrompt: [dice1 convertValueToUnicodeSymbols:dice1Value]];
+                [InputCollector printToPrompt: [dice2 convertValueToUnicodeSymbols:dice2Value]];
+                [InputCollector printToPrompt: [dice3 convertValueToUnicodeSymbols:dice3Value]];
+                [InputCollector printToPrompt: [dice4 convertValueToUnicodeSymbols:dice4Value]];
+                [InputCollector printToPrompt: [dice5 convertValueToUnicodeSymbols:dice5Value]];
+            } else {
+                flag = 1;
+            }
+        }
+
         
         
     }

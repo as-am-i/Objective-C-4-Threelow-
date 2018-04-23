f5380031fc958b2b1052498e2076cb9e72c14b70
Asami Tanii <g033.9521@gmail.com>
2018-04-23 14:35:07 -0700

    Initial Commit

---
+++ Assignment4(Threelow)/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Assignment4(Threelow)
+//
+//  Created by 谷井朝美 on 2018-04-23.
+//  Copyright © 2018 Asami Tanii. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
